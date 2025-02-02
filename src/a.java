c2146f8339e0c75f7bd3731d512f3db20af1b209
sushma <sushmreddy.g526@gmail.com>
2025-02-02 15:44:41 +0530

    commit

---
+++ src/a.java
@@ -0,0 +1,5 @@
+public class a {
+    public static void main(String[] args) {
+        System.out.println("this is a java");
+    }
+}
---
+++ src/c.java
@@ -0,0 +1,5 @@
+public class c {
+    public static void main(String[] args) {
+        System.out.println("this is c java");
+    }
+}
